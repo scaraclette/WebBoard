a38a1f3605ff082dd30a4ffb6f9b1763b83d70a2
Scarlett Halima <scarlett.halima@pacificoffice.com>
2020-02-18 12:50:36 -0800

    Checkpoint updated new_topic to use Django Forms. All tests working.

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -24,8 +24,8 @@ def board_topics(request, board_id):
     return render(request, 'topics.html', context)
 
 def new_topic(request, board_id):
-    board = get_object_or_404(Board, id=board_id)
-    user = User.objects.first() # TODO: get the currently logged in user
+    board = get_object_or_404(Board, pk=board_id)
+    user = User.objects.first()  # TODO: get the currently logged in user
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
@@ -38,13 +38,33 @@ def new_topic(request, board_id):
                 topic=topic,
                 created_by=user
             )
-            return redirect('board_topics', board_id=board.id) # TODO: redirect to the created topic page
-        else:
-            form = NewTopicForm()
+            return redirect('board_topics', board_id=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
 
-        context = {
-            'board':board,
-            'form':form
-        }
-        return render(request, 'new_topic.html', context)
+# def new_topic(request, board_id):
+#     board = get_object_or_404(Board, pk=board_id)
+#     user = User.objects.first() # TODO: get the currently logged in user
+#     if request.method == 'POST':
+#         form = NewTopicForm(request.POST)
+#         if form.is_valid():
+#             topic = form.save(commit=False)
+#             topic.board = board
+#             topic.starter = user
+#             topic.save()
+#             post = Post.objects.create(
+#                 message=form.cleaned_data.get('message'),
+#                 topic=topic,
+#                 created_by=user
+#             )
+#             return redirect('board_topics', board_id=board.id) # TODO: redirect to the created topic page
+#         else:
+#             form = NewTopicForm()
+
+#         context = {
+#             'board':board,
+#             'form':form
+#         }
+#         return render(request, 'new_topic.html', context)
     
